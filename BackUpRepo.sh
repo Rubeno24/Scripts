7f2bccdfdf81b3acec1aab7c7242a0939e31e98a
Ruben <ruben000_2@live.com>
2024-04-12 16:12:48 -0700

    Made some changes to my BackupRepo script so it keeps 3 back ups of our senior project

--- BackUpRepo.sh
+++ BackUpRepo.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-# Define variables
-Repo_Url="https://github.com/nalisonia/CtrlALtElite-.git"        # Use your own github repo url
+# Read the number from the var.txt file
+number=$(<var.txt)
 
-FolderPath="$HOME/Desktop/SeniorProjectBackUp/CtrlALtElite-"        # Folder to delete
+# Increment the number that corresponds to the number with ctrlaltelite folder
+# The number will only go up to 2 since we have 3 folders
+((number++))
 
-Directory_Path="$HOME/Desktop/SeniorProjectBackUp"                 # Set the destination directory
+# Make sure the number doesnt go past 2 and if it does set it to 0
+if ((number > 2)); then
+    number=0
+fi
 
-# Change directory to the destination directory
-cd "$Directory_Path" || { echo "Directory not fouind or incorrect path"; exit 1; }
+# Rewrite the entire file with the updated number
+echo "$number" > var.txt
 
-# Deletes this folder
-rm -rf "$FolderPath"
+# Print the updates number
+echo "The updated number is: $number"
 
-# Initialize a new Git repository
-git init
 
-# Create A new Folder to reclone into it, will have the same name
-mkdir -p "$FolderPath"
+# Variables
 
-# Clone the repository
+# The Url to the repo
+Repo_Url="https://github.com/nalisonia/CtrlALtElite-.git" 
+
+# Path to where our folders are located
+FolderPath="$HOME/Desktop/SeniorProjectBackUp"                     
+
+# Updated Folder Path with a number 0,1,2 in it so it correspsonds to the correct folder
+FolderPathNumber="$HOME/Desktop/SeniorProjectBackUp/CtrlALtElite-$number"
+
+# change directory to where the folders are located
+cd "$FolderPath" || { echo "Directory not found or incorrect path"; exit 1; }
+
+# Remove the folder with the number between 0 and 2 so we can create it again and clone to it
+rm -rf "$FolderPathNumber"
+
+# Create a new folder with the same name
+mkdir -p "$FolderPathNumber"
+    
+# Change directory to the new folder
+cd "$FolderPathNumber" || { echo "Directory not found or incorrect path"; exit 1; }
+
+# Clone the repository into that folder with the number between 0 and 2 in it 
 git clone "$Repo_Url"
 
 # Check if the git clone command was successful
@@ -31,3 +54,6 @@ fi
 echo "Git clone completed successfully."
 
 #once Im done unload plist file launchctl unload ~/Library/LaunchAgents/com.SeniorProjectBackUp.plist
+
+
+
